36cb28a800aaadd581f7d75ed927e32881b12f67
mostafabastawy <mostafa.bastawyy@gmail.com>
2023-04-30 14:44:05 +0300

    blocs updated

--- lib/modules/app/bloc_providers.dart
+++ lib/modules/app/bloc_providers.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/single_child_widget.dart';
 import 'package:task_badr/modules/app/app_cubit/app_cubit.dart';
+import 'package:task_badr/modules/home_layout/home_layout_cubit/home_layout_cubit.dart';
 
 final List<SingleChildWidget> blocProviders = [
   BlocProvider(
     create: (BuildContext context) => AppCubit(),
+  ),
+  BlocProvider(
+    create: (BuildContext context) => HomeLayoutCubit(),
   ),
 ];
